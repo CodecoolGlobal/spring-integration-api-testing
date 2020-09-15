62e50904e103ff9a15d11a066e54a6659d6da88b
praczkowski <p.raczkowski@ocado.com>
2020-09-16 00:12:51 +0200

    Initial commit p2

--- src/main/java/com/raczkowski/springintro/controller/UsersController.java
+++ src/main/java/com/raczkowski/springintro/controller/UsersController.java
@@ -4,13 +4,17 @@ import com.raczkowski.springintro.dto.UserDto;
 import com.raczkowski.springintro.exception.UserNotFoundException;
 import com.raczkowski.springintro.model.User;
 import com.raczkowski.springintro.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparing;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Controller
@@ -29,19 +33,33 @@ public class UsersController {
         return "Hello World";
     }
 
-    @GetMapping(produces = APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public List<UserDto> getAllUsers() {
-        return usersService.getAllUsers().stream()
-                .map(user -> new UserDto(user.getName(), user.getAddress(), user.getPhoneNumber()))
-                .collect(Collectors.toList());
-    }
+//    @GetMapping(produces = APPLICATION_JSON_VALUE)
+//    @ResponseBody
+//    public List<UserDto> getAllUsers() {
+//        return usersService.getAllUsers().stream()
+//                .map(user -> new UserDto(user.getName(), user.getAddress(), user.getPhoneNumber()))
+//                .collect(Collectors.toList());
+//    }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     @ResponseBody
     public UserDto getUser(@PathVariable int id) {
         User user = usersService.getUser(id);
         return new UserDto(user.getName(), user.getAddress(), user.getPhoneNumber());
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "ASC") String order) {
+        Comparator<UserDto> comparator = comparing(UserDto::getName);
+        if (order.equals("DESC")) {
+            comparator = (u1, u2) -> u2.getName().compareTo(u1.getName());
+        }
+
+        return usersService.getAllUsers().stream()
+                .map(user -> new UserDto(user.getName(), user.getAddress(), user.getPhoneNumber()))
+                .sorted(comparator)
+                .collect(Collectors.toList());
     }
 
     @PostMapping
@@ -51,8 +69,9 @@ public class UsersController {
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public void handleException() {
-
+    @ResponseBody
+    public ResponseEntity<String> handleException(UserNotFoundException userNotFoundException) {
+        return new ResponseEntity<>(userNotFoundException.getMessage(), NOT_FOUND);
     }
 
 }
--- src/test/java/com/raczkowski/springintro/UserControllerIntegrationTest.java
+++ src/test/java/com/raczkowski/springintro/UserControllerIntegrationTest.java
@@ -1,22 +1,23 @@
 package com.raczkowski.springintro;
 
-import org.junit.Assert;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.raczkowski.springintro.dto.UserDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ApplicationConfig.class})
@@ -35,24 +36,74 @@ public class UserControllerIntegrationTest {
 
     @Test
     public void should_return_hello_world() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/users/hello"))
+        mockMvc.perform(get("/users/hello"))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$").value("Hello World"))
                 .andReturn();
-
-        Assert.assertEquals("application/json",
-                mvcResult.getResponse().getContentType());
     }
 
     @Test
     public void should_return_all_users() throws Exception {
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.[0].name").value("Przemek Raczkowski"))
                 .andExpect(jsonPath("$.[0].address").value("Kraków, ul. Kijowska 54"))
                 .andExpect(jsonPath("$.[0].phoneNumber").value("733897222"))
                 .andExpect(jsonPath("$.[1].name").value("Tomasz Hajto"))
                 .andExpect(jsonPath("$.[1].address").value("Berlin, Herta StraBe"))
                 .andExpect(jsonPath("$.[1].phoneNumber").value("67782323"));
+    }
+
+    @Test
+    public void should_return_user_for_given_id() throws Exception {
+        mockMvc.perform(get("/users/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.name").value("Przemek Raczkowski"))
+                .andExpect(jsonPath("$.address").value("Kraków, ul. Kijowska 54"))
+                .andExpect(jsonPath("$.phoneNumber").value("733897222"));
+    }
+
+    @Test
+    public void should_sort_users_by_request_param() throws Exception {
+        mockMvc.perform(get("/users").param("order", "ASC"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.[0].name").value("Przemek Raczkowski"))
+                .andExpect(jsonPath("$.[0].address").value("Kraków, ul. Kijowska 54"))
+                .andExpect(jsonPath("$.[0].phoneNumber").value("733897222"))
+                .andExpect(jsonPath("$.[1].name").value("Tomasz Hajto"))
+                .andExpect(jsonPath("$.[1].address").value("Berlin, Herta StraBe"))
+                .andExpect(jsonPath("$.[1].phoneNumber").value("67782323"));
+
+        mockMvc.perform(get("/users").param("order", "DESC"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.[0].name").value("Tomasz Hajto"))
+                .andExpect(jsonPath("$.[0].address").value("Berlin, Herta StraBe"))
+                .andExpect(jsonPath("$.[0].phoneNumber").value("67782323"))
+                .andExpect(jsonPath("$.[1].name").value("Przemek Raczkowski"))
+                .andExpect(jsonPath("$.[1].address").value("Kraków, ul. Kijowska 54"))
+                .andExpect(jsonPath("$.[1].phoneNumber").value("733897222"));
+    }
+
+    @Test
+    public void should_return_created_status_code_when_adding_new_user() throws Exception {
+        mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(convertUserDtoToJson(new UserDto("Andrew Golara", "Chicago", "+557263723"))))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void should_return_not_found_status_code_when_user_does_not_exist_for_given_id() throws Exception {
+        mockMvc.perform(get("/users/{id}", 100))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$").value("User not found with given id: 100"));
+    }
+
+    private String convertUserDtoToJson(UserDto userDto) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(userDto);
     }
 }
