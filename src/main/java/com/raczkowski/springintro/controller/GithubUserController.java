d2d07fe12d280858e77d6a6ec30e66423799a76b
praczkowski <p.raczkowski@ocado.com>
2020-09-30 22:07:14 +0200

    oAuth GithubUserController added

---
+++ src/main/java/com/raczkowski/springintro/controller/GithubUserController.java
@@ -0,0 +1,19 @@
+package com.raczkowski.springintro.controller;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class GithubUserController {
+
+    @GetMapping("/github-user")
+    @ResponseBody
+    public Map<String, Object> getGithubUser(OAuth2AuthenticationToken auth2AuthenticationToken) {
+        return auth2AuthenticationToken.getPrincipal().getAttributes();
+    }
+
+}
